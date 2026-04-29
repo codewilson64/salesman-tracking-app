69e85839ac1d921270ff3c0d84fb51578ff6c18b
codewilson64 <wilsongambit@gmail.com>
2026-04-29 18:42:50 +0700

    revert axios

--- frontend/app/libs/axios.ts
+++ frontend/app/libs/axios.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 import { useAuthStore } from "../stores/authStore";
 
-const BASE_URL =
-  __DEV__
-    ? "http://192.168.1.13:5000/api"
-    : "https://sales-team-tracker-api.onrender.com/api";
-
 export const api = axios.create({
-  baseURL: BASE_URL,
-  timeout: 30000,
+  baseURL: "http://192.168.1.13:5000/api",
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
