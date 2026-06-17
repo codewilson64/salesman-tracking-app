235d463e897aaf523fc086293842ff1d91fdc308
codewilson64 <wilsongambit@gmail.com>
2026-06-17 16:16:05 +0700

    update version to 1.0.1 for testing

--- backend/src/routes/appVersion.route.ts
+++ backend/src/routes/appVersion.route.ts
@@ -4,11 +4,11 @@ const router = Router();
 
 router.get("/", (req, res) => {
   return res.status(200).json({
-    latestVersion: "1.0.0",
+    latestVersion: "1.0.1",
     minimumVersion: "1.0.0",
     message: "A new update is available with bug fixes and improvements.",
     iosUrl: "https://testflight.apple.com/join/svFrzVHd",
-    androidUrl: "https://salesteamtracker.vercel.app/downloads/android",
+    androidUrl: "https://salesteamtracker.vercel.app/download/android",
   });
 });
 
